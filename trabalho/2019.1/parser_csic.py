f14fc707484563aca01cabe0652790d266919f23
Rafael Coelho <rafallcoelho@gmail.com>
2019-06-08 19:14:55 -0300

    Parse requests
    - Omit headers, considering only first line of request and request body;
    - Split first line of request into method, url and http version.

--- trabalho/2019.1/parser_csic.py
+++ trabalho/2019.1/parser_csic.py
@@ -1,4 +1,24 @@
 import sys
 
+
 with open(sys.argv[1]) as datafile:
-    
+    emptylines = 0
+    newrequest = True
+    for line in datafile:
+        if line == '\n':
+            emptylines += 1
+            if emptylines == 2:
+                newrequest = True
+                emptylines = 0;
+        else:
+            if emptylines == 1:
+                request_body = line
+                #print(request_body)
+            elif newrequest:
+                request_type, request_url, request_version = line.split()
+                file_and_parameters = request_url[21:]
+                f = file_and_parameters.split('?')
+                #print("%s %s %s" % (request_type, request_url, request_version))
+                newrequest = False
+
+
