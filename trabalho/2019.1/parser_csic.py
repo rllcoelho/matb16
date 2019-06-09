aa93a4ea8ccfb800e20a7b9fa24f808ba88835bf
Rafael Coelho <rafallcoelho@gmail.com>
2019-06-09 01:41:27 -0300

    Parsing finished:

--- trabalho/2019.1/parser_csic.py
+++ trabalho/2019.1/parser_csic.py
@@ -1,11 +1,8 @@
 import sys
 
-def sub_file_paths(file_path):
-    pass
-
 def parse_parameter(p):
     name, value = p.rstrip().split('=') if p.find('=') != -1 else (p, None)
-    words = value.split('+') if value else None
+    words = value.split('+') if value else []
     return name, words
 
 def parse_parameters(params):
@@ -20,17 +17,23 @@ def parse_body(b):
 
 def parse_url(url, uselesspart = 'http://localhost:8080'):
     path_and_parameters = url[len(uselesspart):]
-    path, parameters = path_and_parameters.split('?') if path_and_parameters.find('?') != -1 else (path_and_parameters, None)
+    path, parameters = path_and_parameters.split('?') if path_and_parameters.find('?') != -1 else (path_and_parameters, [])
     path_parts = path.split('/')
-    l_parameters = parse_parameters(parameters) if parameters else None
-    params_names = set()
-    return params_names
+    l_parameters = parse_parameters(parameters) if parameters else []
+    return path_parts, l_parameters
+
+def parse_request(r):
+    method, url, version = r.split()
+    
+    path, parameters = parse_url(url)
+
+    return {'method': method, 'path': path, 'parameters': parameters, 'version': version} 
 
 def main():
     emptylines, count = (0, 0)
     newrequest = True
-    params_names = set()
-    
+    requests = []
+
     with open(sys.argv[1]) as datafile:# {{{
         for line in datafile:
             if line == '\n': # If empty line
@@ -40,16 +43,12 @@ def main():
                     emptylines = 0;
             else:
                 if emptylines == 1: # If a line isn't empty but the last one was, it is the request body
-                    request_body = line
-                    print(parse_body(request_body))
+                    requests[-1]['parameters'].append(parse_body(line))
                 elif newrequest:
-                    request_type, request_url, request_version = line.split() # If it's the first line of a request, parse it.
-                    count += 1
-                    params_names = params_names.union(parse_url(request_url))
-                    #print("%s %s %s" % (request_type, request_url, request_version))
+                    requests.append(parse_request(line)) # If it's the first line of a request, parse it.
                     newrequest = False
-
-        #print(count)
+    for i in range(10):
+        print(requests[i])
     # }}}
 
 main()
